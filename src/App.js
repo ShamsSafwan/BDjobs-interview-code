f1b49f7653b1b045a6b296cf66637f402d929aa7
ShamsSafwan <41986366+ShamsSafwan@users.noreply.github.com>
2023-04-03 18:18:05 +0000

    some edit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <Navbar bg="primary" variant="dark" expand="lg">
-      <Navbar.Brand href="#">My App</Navbar.Brand>
+      <Navbar.Brand className="mx-auto pl-40 " href="#">My App</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
